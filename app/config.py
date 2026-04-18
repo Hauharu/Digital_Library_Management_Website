f99bd7172a874f1ff6988aa25059ff0ea9870d94
thuanvo2004 <thuanprozz2004@gmail.com>
2026-04-19 00:20:02 +0700

    OL-50

---
+++ app/config.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+import stripe
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', '12b23b02a8ceeb2cf3386bb33d4d8f31')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI',
+        'mysql+pymysql://root:admin@localhost:3306/thu_vien_so'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
+
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
+
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+
+
+    GOOGLE_OAUTH_CONFIG = {
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET,
+        'access_token_url': 'https://oauth2.googleapis.com/token',
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'api_base_url': 'https://www.googleapis.com/oauth2/v1/',
+        'client_kwargs': {'scope': 'openid email profile'},
+        'server_metadata_url': 'https://accounts.google.com/.well-known/openid-configuration',
+        "redirect_uri": "http://127.0.0.1:5000/auth/google/callback"
+    }
+
+
+    GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+
--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
+from app import create_app,db
 
-app = Flask(__name__)
+app = create_app()
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
